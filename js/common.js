ece8bec1a133314fa8e0b4b4043090440ee6cb48
Kristina Yancer <kristy.net.ua@mail.ru>
2025-11-22 00:20:30 +0300

    fix

--- js/common.js
+++ js/common.js
@@ -1,28 +1,54 @@
 $(function() {
 
     // Burger Menu
-    const body = document.querySelector("body");
     const header = document.querySelector(".header");
     const burgerMenu = document.querySelector(".header .header-button-menu");
-    const burgerBody = document.querySelector(".header .header-button-menu__body");
+    const burgerBody = document.querySelector(".header .header-button-menu-body");
 
 
     burgerMenu.addEventListener("click", (e) => {
     header.classList.toggle("header-menu-open");
     burgerBody.classList.toggle("active");
-    body.classList.toggle("lock");
     e.stopPropagation();
 
 
     if($('.header-menu-more').hasClass('active')) {
         $(".header").addClass("header-menu-open");
-        $(".header .header-button-menu__body").addClass("active");
+        $(".header .header-button-menu-body").addClass("active");
     }
     });
 
-    $(".header-top__socials").clone().appendTo(".header-button-menu__socials");
-    $(".header-top__phone").clone().appendTo(".header-button-menu__socials");
-    $(".main-menu").clone().appendTo(".header-button-menu__main-menu");
+    $(".header-top__socials").clone().appendTo(".header-button-menu-body__socials");
+    $(".header-top__phone").clone().appendTo(".header-button-menu-body__socials");
+    $(".main-menu").clone().appendTo(".header-button-menu-body__main-menu");
+    $(".search").clone().prependTo(".search-mobile__input");
 
+    
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+  const searchMobile = document.querySelector('.search-mobile__icon');
+  const searchElement = document.querySelector('.search-mobile__input');
+
+  searchMobile?.addEventListener('click', (e) => {
+    e.stopPropagation();
+    searchElement?.classList.toggle('active');
+    
+    if (searchElement?.classList.contains('active')) {
+      searchElement.focus();
+    }
+  });
+
+  searchElement?.addEventListener('click', (e) => {
+    e.stopPropagation();
+  });
+
+  document.addEventListener('click', (e) => {
+    const isIcon = e.target === searchMobile;
+    const isInput = e.target === searchElement || searchElement?.contains(e.target);
+
+    if (!isIcon && !isInput) {
+      searchElement?.classList.remove('active');
+    }
+  });
+});
