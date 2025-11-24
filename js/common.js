2b388821e468b783eddb544bca645bf556f2cb39
Kristina Yancer <kristy.net.ua@mail.ru>
2025-11-24 14:45:30 +0300

    fix

--- js/common.js
+++ js/common.js
@@ -347,10 +347,6 @@ $(function() {
 
 
 
-
-
-
-
 // Search Button
 document.addEventListener('DOMContentLoaded', () => {
     const searchMobile = document.querySelector('.search-mobile__icon');
@@ -377,4 +373,66 @@ document.addEventListener('DOMContentLoaded', () => {
             searchElement?.classList.remove('active');
         }
     });
+});
+
+
+
+
+// Accordion
+let Accordion = function (el, multiple) {
+  this.el = el || {};
+  // more then one submenu open?
+  this.multiple = multiple || false;
+
+  let accordionMenuLink = this.el.find('.accordion-menu__link');
+  accordionMenuLink.on('click',
+    { el: this.el, multiple: this.multiple },
+    this.dropdown);
+};
+
+Accordion.prototype.dropdown = function (e) {
+  let $el = e.data.el,
+    $this = $(this),
+    //this is the ul.accordion-menu__content
+    $next = $this.next();
+
+  $next.slideToggle();
+  $this.parent().toggleClass('open');
+
+  if (!e.data.multiple) {
+    //show only one menu at the same time
+    $el.find('.accordion-menu__content').not($next).slideUp().parent().removeClass('open');
+  }
+}
+
+let accordion = new Accordion($('.accordion-menu'), false);
+
+
+
+// Show all button
+$('.bookmakers-block__button').click(function (e) {
+  e.preventDefault();
+  e.returnValue = false;
+  
+  let length_before_toggle = $('.bookmakers-block-list__item:hidden').length;
+  
+  $('.bookmakers-block-list__item:hidden').slice(0, 10).toggle();
+  
+  if ($('.bookmakers-block-list__item:hidden').length == 0) {
+    if (length_before_toggle == 0) {
+      $('.bookmakers-block-list__item--hidden').toggle();
+      
+      $('html, body').animate({
+        scrollTop: $('.bookmakers-block').offset().top
+      }, 'slow');
+      
+      $('.bookmakers-block__button-text').text("Показать еще");
+      $('.bookmakers-block__button').removeClass('hide');
+    } else {
+      $('.bookmakers-block__button-text').text("Скрыть");
+      $('.bookmakers-block__button').addClass('hide');
+    }
+  } else {
+    $('.bookmakers-block__button-text').text("Показать еще");
+  }
 });
