41ca24622eba95ebfd2fffa8691770327501e676
Kristina Yancer <kristy.net.ua@mail.ru>
2026-01-11 18:47:07 +0300

    fix

--- js/common.js
+++ js/common.js
@@ -490,6 +490,16 @@ $(function() {
   }
 
 
+  //=== Слайдер Зоны
+  $('.cosmetology-info-slider').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: true,
+      arrows: false,
+      infinite: true
+  });
+
+
 
 
 
